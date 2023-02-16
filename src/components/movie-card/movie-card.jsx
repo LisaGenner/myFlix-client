64da10b9fedde3f05305f94589b5f4cd9fcac015
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-02-15 20:36:04 -0600

    Create movie-card.jsx
    new movie-card file 3.3

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,11 @@
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+        <div
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
+};
