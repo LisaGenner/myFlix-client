23efc14ecd15799af718833f46b186d200e744ce
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-02-16 15:30:14 -0600

    Update index.jsx
    updated code per 3.3

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,20 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
-
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+//commented out this code to see if i can get movie cards to show in local host
 // Main component (will eventually use all the others)
-const MyFlixApplication = () => {
-    return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
-    );
+// const MyFlixApplication = () => {
+//     return (
+//         <div className="my-flix">
+//             <div>Good morning</div>
+//         </div>
+//     );
+// };
+
+const App = () => {
+    return <MainView />;
 };
 
 // Finds the root of your app
@@ -18,4 +22,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<App />);
