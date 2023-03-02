b73895e470163f69b4eba265811de821398dd1a3
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-03-01 18:34:18 -0600

    Update main-view.jsx
    updated main-view with updated login

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,63 @@
-import { React, useState, useEffect } from "react";
-//import { title } from "process";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        fetch("https://myflix-20778.herokuapp.com/movies")
+        if (!token)
+            return;
+
+        fetch("https://myflix-20778.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then((response) => response.json())
-            .then((data) => {
-                // console.log(data);
-                const movieFromApi = data.map((doc) => {
-                    const actors = doc.Actors
-                    return {
-                        id: doc._id,
-                        title: doc.Title,
-                        imagepath: doc.ImagePath,
-                        director: doc.Director.Name,
-                        actors: actors
-                    };
-                });
-
-                setMovies(data);
-                // console.log(movieFromApi)
+            .then((movies) => {
+                setMovies(movies);
             });
-    }, []);
+    }, [token]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    console.log(movies);
+    const movieFromApi = movies.map((doc) => {
+        const actors = doc.Actors
+        return {
+            id: doc._id,
+            title: doc.Title,
+            imagepath: doc.ImagePath,
+            director: doc.Director.Name,
+            actors: actors
+        };
+    });
+
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }} />
+                or
+                <SignupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            <>
+                <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+                <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)}
+                />
+            </>
         );
     }
 
