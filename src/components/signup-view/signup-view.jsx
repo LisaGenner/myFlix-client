917de35cc9e8a3cdc573c021166ad2fd148196bc
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-03-01 18:34:52 -0600

    Create signup-view.jsx
    added signup-view tab

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export const SignupView = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const handleSubmit = (event) => { };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength="3"
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Birthday:
+                <input
+                    type="date"
+                    value={birthday}
+                    onChange={(e) => setBirthday(e.target.value)}
+                    required
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
