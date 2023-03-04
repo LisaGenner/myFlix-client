921baf808de2921c74692e4839a9c3e8b3a9d093
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-03-03 18:26:08 -0600

    Update index.jsx
    updates for 3.6

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,27 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
-
-//commented out this code to see if i can get movie cards to show in local host
-// Main component (will eventually use all the others)
-// const MyFlixApplication = () => {
-//     return (
-//         <div className="my-flix">
-//             <div>Good morning</div>
-//         </div>
-//     );
-// };
+import Container from "react-bootstrap/Container";
 
 const App = () => {
-    return <MainView />;
+    return (
+        <Container>
+            <MainView />
+        </Container>
+    );
+
+
+    //commented out this code to see if i can get movie cards to show in local host
+    // Main component (will eventually use all the others)
+    // const MyFlixApplication = () => {
+    //     return (
+    //         <div className="my-flix">
+    //             <div>Good morning</div>
+    //         </div>
+    //     );
+    // };
+
+
 };
 
 // Finds the root of your app
