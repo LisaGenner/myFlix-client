d7c1fccba2f9c0c07b436b6fe5ed047284cddc03
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2023-03-09 11:27:41 -0600

    implemented Nav

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,20 @@
 //Here you import the proptypes library
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 
 
 //The movieCard function component
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.ImagePath} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
                 <Card.Text>{movie.director}</Card.Text>
-                <Button onClick={() =>
-                    onMovieClick(movie)} variant="link">
-                    Open
-                </Button>
+                //<Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                    <Button variant="link">Open</Button>
+                </Link>
             </Card.Body>
         </Card>
     );
@@ -28,6 +27,6 @@ MovieCard.propTypes = {
         title: PropTypes.string.isRequired,
         genre: PropTypes.string.isRequired,
         director: PropTypes.string.isRequired
-    }),
-    onMovieClick: PropTypes.func.isRequired
+    })
+        .isRequired
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,48 +1,56 @@
 // import PropTypes from "prop-types";
-// import "./movie-view.scss";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Row, Col } from 'react-bootstrap';
+import { MovieCard } from '../movie-card/movie-card';
+import "./movie-view.scss";
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies }) => {
+    const { movieId } = useParams();
+    const movie = movies.find((m) => m.id === movieId);
+    console.log(movies)
     return (
         <div>
             <div>
-                <img className="w-100" src={movie.ImagePath} />
+                <img className="w-100" src={movies.ImagePath} />
             </div>
             <div>
                 <span>Title:</span>
-                <span>{movie.Title} </span>
+                <span>{movies.Title} </span>
             </div>
-            <div>
+            {/* <div>
                 <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
+                <span>{movies.Genre.Name}</span>
+            </div> */}
+            {/* <div>
                 <span>Director: </span>
-                <span>{movie.Director.Name}</span>
+                <span>{movies.Director.Name}</span>
             </div>
             <div>
                 <span>Actors:</span>
-                <span>{movie.Actors}</span>
+                <span>{movies.Actors}</span>
             </div>
             <div>
                 <span>Description:</span>
-                <span>{movie.Description.Name}</span>
-            </div>
+                <span>{movies.Description.Name}</span>
+            </div> */}
 
-            <button onClick={onBackClick} className="back-button"
-                style={{ cursor: "pointer" }}
-            >
-                Back
-            </button>
+            <Link to={`/`}>
+                <button className="back-button">Back</button>
+            </Link>
 
         </div>
     );
 };
 
-// MovieView.propTypes = {
+// MovieCard.propTypes = {
 //     movie: PropTypes.shape({
 //         title: PropTypes.string.isRequired,
 //         image: PropTypes.string.isRequired,
+//         genre: PropTypes.string.isRequired,
 //         director: PropTypes.string.isRequired
-//     }).isRequired,
-//     onBackClick: PropTypes.func.isRequired
+//     })
+//     .isRequired
 // };
